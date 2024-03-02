88fb12a2ebc413c10008c07b917b8036eff4896d
manikanta700 <20pa1a5455@vishnu.edu.in>
2024-03-02 21:26:15 +0530

    v2

--- mycustomerswebapp/src/index.js
+++ mycustomerswebapp/src/index.js
@@ -10,8 +10,7 @@ root.render(
     <link
   href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
   rel="stylesheet"
-  integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-  crossorigin="anonymous"
+
 />
     <App />
   </React.StrictMode>
